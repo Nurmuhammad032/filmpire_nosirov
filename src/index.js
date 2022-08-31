ad60fde639f0a4b816a51e817c7c788e3c292bba
Nurmuhammad <nurmuhammad2003o@gmail.com>
2022-08-31 17:41:43 +0500

    Navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material';
 import App from './components/App';
+
+const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 );
